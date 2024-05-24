b95b26d6ea416e18311d3786c8150cf3198a6ca8
anushpathak09 <anushpathak412@gmail.com>
2024-05-24 22:52:40 +0545

    Third Commit

---
+++ build/Debug/Basics of C++/FindASCIIvalue.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     cout<<"Enter character: ";
+//     cin>>ch;
+//     cout<<(int)ch;
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    int a= (char)ch;
+    cout<<a;
+}
